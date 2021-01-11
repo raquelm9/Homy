4da75a7d2364d051ba3cb58ad867d95129afe566
Raquel Mijares <raquelm9@hotmail.com>
2021-01-11 15:03:32 -0700

    routing setup

---
+++ client/src/app/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Pages
+import MainPage from "../pages/MainPage/MainPage";
+import ResidentRequest from "../pages/ResidentRequest/ResidentRequest";
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+        <Route exact path="/resident-request" component={ResidentRequest} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ client/src/pages/ResidentRequest/ResidentRequest.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ResidentRequest() {
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default ResidentRequest;
