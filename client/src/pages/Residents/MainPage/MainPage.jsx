1d0fa8d7b20789f2fc18bfaca44c7ecfd002fd27
sadybekov <sadybekov@gmail.com>
2021-01-17 22:48:36 -0700

    cleanup

--- client/src/pages/Residents/MainPage/MainPage.jsx
+++ client/src/pages/Residents/MainPage/MainPage.jsx
@@ -1,11 +1,6 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Container";
-import Col from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
-import mainImage from "../../../assets/main1.jpg";
-import { Jumbotron } from "../../../components/Jumbotron";
+import { Container, Button} from "react-bootstrap";
+import { LoginPageImage } from "../../../components/LoginPageImage.js";
 import { useHistory } from "react-router-dom";
 import "./MainPage.css";
 
@@ -16,19 +11,16 @@ function MainPage() {
 
   return (
     <>
-      <Container className="container" fluid xs={12} xl={6}>
-        <Jumbotron className="jumbotron"></Jumbotron>
-        <Row className="text-center buttonRow">
-          <Col xl={12}>
-            <Button
-              className="buttonGetStarted"
-              variant="dark"
-              onClick={goToServiceRequest}
-            >
-              Get Started
-            </Button>
-          </Col>
-        </Row>
+      <Container fluid>
+        <LoginPageImage/>
+        <div className="LoginPageBottom">
+          <Button
+                className="buttonGetStarted"
+                variant="dark"
+                onClick={goToServiceRequest}>
+                Get Started
+          </Button>
+        </div>
       </Container>
     </>
   );
