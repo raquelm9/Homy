8a35171381132228b9cb2b2ec45185c02e8efbce
ptrickd <patrickdion8@gmail.com>
2021-01-20 15:38:32 -0700

    added navbar login logout

---
+++ client/src/components/Layouts/ResidentNavbar.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logUserOut } from '../../actions/userActions';
+
+import './ResidentNavbar.css'
+
+function ResidentNavbar() {
+    const dispatch = useDispatch();
+    const loggedIn = useSelector(state => state.userReducer.loggedIn)
+    const location = useLocation();
+
+    return (
+        <>
+            {
+                location.pathname === '/' ? null :
+                    < div className="navbar-container" >
+                        <nav className="navbar navbar-light bg-light">
+                            <ul className="navbar-nav mr-auto">
+                                {
+                                    loggedIn ?
+                                        <li className="nav-item">
+                                            <Link class="nav-link " to="/" onClick={() => dispatch(logUserOut())}>Logout</Link>
+                                        </li> :
+                                        <li className="nav-item">
+                                            <Link class="nav-link " to="/login" >Login</Link>
+                                        </li>
+                                }
+                            </ul>
+                        </nav>
+                    </div >
+            }
+        </>
+    )
+}
+
+export default ResidentNavbar
