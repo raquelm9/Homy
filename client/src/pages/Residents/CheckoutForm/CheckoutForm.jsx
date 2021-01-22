3b16a727535e8a349f9f8ea1513c2d226b7dcf5d
ptrickd <patrickdion8@gmail.com>
2021-01-22 08:45:01 -0700

    added valid fetch to payment method

--- client/src/pages/Residents/CheckoutForm/CheckoutForm.jsx
+++ client/src/pages/Residents/CheckoutForm/CheckoutForm.jsx
@@ -1,48 +1,51 @@
 
-import {CardElement, useStripe, useElements, Elements} from '@stripe/react-stripe-js'
-import {loadStripe } from '@stripe/stripe-js'
+import { CardElement, useStripe, useElements, Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 
 
 const CheckoutForm = () => {
-const stripe = useStripe();
-const elements = useElements();
-// const stripePromise = loadStripe(
-//     "pk_test_51IBDyvF2HDM8CiHYQJfsy5btrP7rN4aZ6jgN1iSx8DYAXcOlbpxVeDpOJ0zZUCFTJwwV6zjBirsx2RHoOQLGJizj00NqqTs3Bx"
+    const stripe = useStripe();
+    const elements = useElements();
+    // const stripePromise = loadStripe(
+    //     "pk_test_51IBDyvF2HDM8CiHYQJfsy5btrP7rN4aZ6jgN1iSx8DYAXcOlbpxVeDpOJ0zZUCFTJwwV6zjBirsx2RHoOQLGJizj00NqqTs3Bx"
 
-// )
-const handleSubmit = async event => {
-    event.preventDefault()
-    if (!stripe || !elements)return
-const cardElement = elements.getElement(CardElement)
+    // )
+    const handleSubmit = async event => {
+        event.preventDefault()
+        if (!stripe || !elements) return
+        const cardElement = elements.getElement(CardElement)
 
-const {error, paymentMethod} = await stripe.createPaymentMethod({
-    type:'card',
-    card: cardElement
-})
-if (error){
-    console.log('error',error)
-}
-else{
-    console.log('PaymentMethod', paymentMethod)
-    //fetch('http://localhost:4242/create-checkout-session',{
-        //method:'POST',
-      //  body: JSON.stringify(paymentMethod)
-    //})
-}
-}
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardElement
+        })
+        if (error) {
+            console.log('error', error)
+        }
+        else {
+            console.log('PaymentMethod', paymentMethod)
+            fetch('http://localhost:3008/shop/pay', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ payment_method_id: paymentMethod.id })
+            })
+        }
 
-    return(
-        <div>
-                
-<form onSubmit={handleSubmit}>
-                     <CardElement />
+        return (
+            <div>
+
+                <form onSubmit={handleSubmit}>
+                    <CardElement />
                     <button type="submit" disabled={!stripe}>
-                Pay
+                        Pay
             </button>
-        </form>
-                     
-        </div>
-    )
-};
+                </form>
+
+            </div>
+        )
+    };
+}
 
 export default CheckoutForm;
