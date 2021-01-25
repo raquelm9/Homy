44853ef9e2d294bcfbfca5ebcd392e9920516087
Raquel Mijares <raquelm9@hotmail.com>
2021-01-24 18:35:01 -0700

    navbar adjusted

--- client/src/components/Layouts/ResidentNavbar.js
+++ client/src/components/Layouts/ResidentNavbar.js
@@ -13,29 +13,66 @@ function ResidentNavbar() {
   return (
     <>
       {location.pathname === "/" ? null : (
-        <div className="navbar-container">
-          <nav className="navbar navbar-light bg-light">
-            <ul className="navbar-nav mr-auto">
-              {loggedIn ? (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              Homy
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarText"
+              aria-controls="navbarText"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link
-                    className="nav-link "
-                    to="/"
-                    onClick={() => dispatch(logUserOut())}
-                  >
-                    Logout
-                  </Link>
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
                 </li>
-              ) : (
-                  <li className="nav-item">
-                    <Link className="nav-link " to="/login">
-                      Login
-                  </Link>
-                  </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+              </ul>
+              <span className="navbar-text">
+                {loggedIn ? (
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link
+                        className="nav-link"
+                        to="/"
+                        onClick={() => dispatch(logUserOut())}
+                      >
+                        Logout
+                      </Link>
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="navbar-nav">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/login">
+                        Login
+                      </Link>
+                    </li>
+                  </ul>
                 )}
-            </ul>
-          </nav>
-        </div>
+              </span>
+            </div>
+          </div>
+        </nav>
       )}
     </>
   );
