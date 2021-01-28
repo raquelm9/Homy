a4bac30fe35bc50e36d9036bf89916e6020e5dfe
ptrickd <patrickdion8@gmail.com>
2021-01-28 08:37:55 -0700

    added delete order manager side

--- client/src/components/MngrEachOrder/MngrEachOrder.js
+++ client/src/components/MngrEachOrder/MngrEachOrder.js
@@ -1,19 +1,19 @@
 import React from "react";
 // import DetailModal from "../DetailModal/DetailModal";
-// import { useHistory } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 function ResidentEachService(props) {
     // const modalId = `request-${props.id}`;
-    // const history = useHistory();
-    // const handleOnClick = () => {
-    //     fetch(`http://localhost:3008/api/service-requests/${props.id}`, {
-    //         method: "DELETE",
-    //         headers: {
-    //             "x-auth-token": `${localStorage.getItem("token")}`,
-    //         },
-    //     });
-    //     history.go(0);
-    // };
+    const history = useHistory();
+    const handleOnClick = () => {
+        fetch(`http://localhost:3008/api/shop/orders/${props.id}`, {
+            method: "DELETE",
+            headers: {
+                "x-auth-token": `${localStorage.getItem("token")}`,
+            },
+        });
+        history.go(0);
+    };
 
     const handleDate = () => {
 
@@ -32,6 +32,14 @@ function ResidentEachService(props) {
                 <td>{props.type}</td>
                 <td>{props.name}</td>
                 <td>{props.unit}</td>
+                <td>
+                    <button
+                        onClick={handleOnClick}
+                        type="button"
+                        className="btn btn-dark"
+                    >
+                        Fulfilled
+                        </button></td>
                 {/* <td>
                     <button
                         type="button"
