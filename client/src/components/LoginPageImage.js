f4677a47dfbd5d74ecd4d7961b39a7b57278f399
sadybekov <sadybekov@gmail.com>
2021-02-22 09:35:13 -0700

    Main page

--- client/src/components/LoginPageImage.js
+++ client/src/components/LoginPageImage.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 export const LoginPageImage = () => (
-  <div className="mainPageLogo">
-    <p className="homyText">homy</p>
-    <p className="homyTextIntro">Your very personal</p>
-    <p className="homyTextIntro">concierge</p>
+  <div className="mainPage__logo">
+    <p className="homy__logo">homy</p>
+    <p className="homy__intro__text">Your very personal</p>
+    <p className="homy__intro__text">concierge</p>
   </div>
 );
 
